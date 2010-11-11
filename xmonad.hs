b5dd77f6b03758cfc6ea79c7beb67b3320532b8b
E.Azer Koçulu <azer@kodfabrik.com>
2010-11-11 15:59:34 +0200

    add volume widget, tab layout

--- xmonad.hs
+++ xmonad.hs
@@ -13,6 +13,13 @@ import qualified XMonad.Actions.Search as S
 import qualified XMonad.Actions.Submap as SM
 import qualified XMonad.Prompt         as P
 
+import XMonad.Layout.WindowNavigation
+import XMonad.Layout.Tabbed
+import XMonad.Layout.TwoPane
+import XMonad.Layout.MosaicAlt
+import XMonad.Layout.Combo
+import XMonad.Layout.WindowNavigation
+
 myManageHook = composeAll
     [
       className =? "Emacs"            --> doF (W.shift "HOME"),
@@ -27,6 +34,19 @@ myManageHook = composeAll
 myKeys = 
     [ 
       ((modMask, xK_s), search)
+       -- multimedia keys
+       -- XF86AudioLowerVolume
+       , ((0            , 0x1008ff11), spawn "ossmix -- vmix0-outvol -2")
+       -- XF86AudioRaiseVolume
+       , ((0            , 0x1008ff13), spawn "ossmix vmix0-outvol +2")
+       -- XF86AudioMute
+       , ((0            , 0x1008ff12), spawn "amixer -q set PCM toggle")
+       -- XF86AudioNext
+       , ((0            , 0x1008ff17), spawn "mocp -f")
+       -- XF86AudioPrev
+       , ((0            , 0x1008ff16), spawn "mocp -r")
+       -- XF86AudioPlay
+       , ((0            , 0x1008ff14), spawn "mocp -G")
     ]
     where modMask     = mod4Mask
           modShft     = modMask .|. shiftMask
@@ -43,13 +63,15 @@ myKeys =
                 ((nilMask, xK_i), m S.imdb)
               ]
 
+myTabConfig = defaultTheme { inactiveColor = "#050505", activeColor = "#050505",  inactiveBorderColor = "#050505", inactiveTextColor = "#666666", activeBorderColor = "#050505", activeTextColor = "#eeeeee"}
+
 main = do
     putEnv "BROWSER=w3"
     xmproc <- spawnPipe "xmobar $HOME/.xmonad/xmobar"
     xmonad $ defaultConfig { 
         -- basic conf
         modMask            = mod4Mask,
-        terminal           = "rxvt-unicode",
+        terminal           = "xterm",
         borderWidth        = 2,
         workspaces         = ["HOME","DEV","IRC","WWW"] ++ map show [5..9],
         -- colors
@@ -57,9 +79,14 @@ main = do
         focusedBorderColor = "#111111",
         -- hooks
         manageHook = myManageHook <+> manageDocks,
-        layoutHook = avoidStruts  $  layoutHook defaultConfig, 
+        -- layoutHook = avoidStruts $ layoutHook defaultConfig,
+        layoutHook      = windowNavigation $ (avoidStruts (tall ||| Mirror tall ||| tabbed shrinkText myTabConfig ||| Full)),
         logHook = dynamicLogWithPP $ xmobarPP
                         { ppOutput = hPutStrLn xmproc
                         , ppTitle = xmobarColor "green" "" . shorten 50
                         } 
         } `additionalKeys` myKeys
+        where 
+          tall  = Tall 1 (3/100) (488/792)
+          -- mosaic  = MosaicAlt M.empty
+          -- combo   = combineTwo (TwoPane 0.03 (3/10)) (mosaic) (Full)
